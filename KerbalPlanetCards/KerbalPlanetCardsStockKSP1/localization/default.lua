c87c2ce3233c070f11b53c969eb6ebebbba149df
PSRPulsar <jimmyhabraken@gmail.com>
2025-04-04 22:27:53 -0400

    Add files via upload

---
+++ KerbalPlanetCards/KerbalPlanetCardsStockKSP1/localization/default.lua
@@ -0,0 +1,64 @@
+return {
+    misc = {
+        dictionary = {
+            k_moon = 'Moon',
+        }
+    },
+    descriptions = {
+        alt_texture = {
+            alt_tex_kpcs_planets = {
+                c_mercury = {
+                    name = 'Moho',
+                },
+                c_venus = {
+                    name = 'Eve',
+                },
+                c_earth = {
+                    name = 'Kerbin',
+                },
+                c_mars = {
+                    name = 'Duna',
+                },
+                c_jupiter = {
+                    name = 'Jool',
+                },
+                c_saturn = {
+                    name = 'Laythe',
+                    badge = 'badge_moon',
+                },
+                c_uranus = {
+                    name = 'Vall',
+                    badge = 'badge_moon',
+                },
+                c_neptune = {
+                    name = 'Tylo',
+                    badge = 'badge_moon',
+                },
+                c_pluto = {
+                    name = 'Eeloo',
+                },
+                c_planet_x = {
+                    name = 'Gilly',
+                    badge = 'badge_moon',
+                },
+                c_ceres = {
+                    name = 'Dres',
+                },
+                c_eris = {
+                    name = 'Ike',
+                    badge = 'badge_moon',
+                },
+                name = 'Planet Cards'
+            },
+        },
+        texture_packs = {
+            loc_txt = {
+                name = 'Stock KSP1',
+                text = {
+                    'Replaces planet cards with planets',
+                    'from stock Kerbal Space Program 1'
+                }
+            }
+        }
+    }
+}
