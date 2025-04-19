771eb329fedfd7ae89126ce2e1c344ec0fae6fea
PSRPulsar <jimmyhabraken@gmail.com>
2025-04-19 17:27:35 -0400

    Add files via upload

---
+++ KerbalPlanetCardsOPMMPE/localization/default.lua
@@ -0,0 +1,60 @@
+return {
+    misc = {
+        dictionary = {
+            k_dwarfplanet = 'Dwarf Planet',
+        }
+    },
+    descriptions = {
+        alt_texture = {
+            alt_tex_kpco_planets = {
+                c_mercury = {
+                    name = 'Moho',
+                },
+                c_venus = {
+                    name = 'Eve',
+                },
+                c_earth = {
+                    name = 'Kerbin',
+                },
+                c_mars = {
+                    name = 'Duna',
+                },
+                c_jupiter = {
+                    name = 'Jool',
+                },
+                c_saturn = {
+                    name = 'Sarnus',
+                },
+                c_uranus = {
+                    name = 'Urlum',
+                },
+                c_neptune = {
+                    name = 'Neidon',
+                },
+                c_pluto = {
+                    name = 'Plock',
+                },
+                c_planet_x = {
+                    name = 'Soden',
+                    badge = 'badge_dwarfplanet'
+                },
+                c_ceres = {
+                    name = 'Dres',
+                },
+                c_eris = {
+                    name = 'Ervo',
+                },
+                name = 'Planet Cards'
+            },
+        },
+        texture_packs = {
+            loc_txt = {
+                name = 'OPM + MPE',
+                text = {
+                    'Replaces planet cards with planets',
+                    'from stock KSP1, OPM, and MPE'
+                }
+            }
+        }
+    }
+}
---
+++ KerbalPlanetCardsOPMMPE/main.lua
@@ -0,0 +1,70 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Kerbal Planet Cards - OPM + MPE
+--- MOD_ID: kplanetcardsopm
+--- MOD_AUTHOR: [PSRPulsar]
+--- MOD_DESCRIPTION: Replaces planet cards with the planets from stock Kerbal Space Program 1, Outer Planets Mod, and Minor Planets Expansion
+--- PREFIX: kpco
+--- VERSION: 1.0.0
+--- DEPENDENCIES: [malverk]
+
+Malverk.badges.badge_dwarfplanet = function(self, card, badges)
+    badges[#badges + 1] = create_badge(localize('k_dwarfplanet'), get_type_colour(self or card.config, card), nil, 1.2)
+end
+
+AltTexture{ 
+    key = 'planets',
+    set = 'Planet',
+    path = 'opm-tarot.png',
+    original_sheet = true,
+    display_pos = 'c_saturn',
+    localization = true
+}
+AltTexture{ 
+    key = 'boosters',
+    set = 'Booster',
+    original_sheet = true,
+    path = 'opm-booster.png',
+    loc_txt = {name = 'Celestial Packs'},
+    keys = {
+        'p_celestial_normal_1',
+        'p_celestial_normal_2',
+        'p_celestial_normal_3',
+        'p_celestial_normal_4',
+        'p_celestial_jumbo_1',
+        'p_celestial_jumbo_2',
+        'p_celestial_mega_1',
+        'p_celestial_mega_2',
+    },
+}
+AltTexture{
+    key = 'spectral',
+    set = 'Spectral',
+    path = 'opm-tarot.png',
+    original_sheet = true,
+    keys = {'c_black_hole'},
+    loc_txt = {name = 'Black Hole'},
+}
+AltTexture{
+    key = 'joker',
+    set = 'Joker',
+    path = 'opm-joker.png',
+    original_sheet = true,
+    keys = {'j_astronomer'},
+    loc_txt = {name = 'Astronomer'},
+}
+TexturePack{ 
+    key = 'kspcards',
+    textures = {
+        'kpco_planets',
+        'kpco_spectral',
+        'kpco_joker',
+        'kpco_boosters',
+    },
+    loc_txt = {
+        name = 'OPM + MPE',
+        text = {
+             'Replaces planet cards with planets',
+               'from stock KSP1, OPM, and MPE'
+        }
+    }
+}
